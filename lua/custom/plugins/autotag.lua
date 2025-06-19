e703302a36e8bf27a12d7a330da2a65151b3e973
Andres Matta <amttdev@gmail.com>
2025-06-19 16:23:10 -0500

    fix: deprecated autotag config

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -1,8 +1,13 @@
 return {
   'windwp/nvim-ts-autotag',
-  opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true, -- Auto close on trailing </
-  },
+  config = function()
+    require('nvim-ts-autotag').setup {
+      autotag = {
+        enable = true,
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    }
+  end,
 }
